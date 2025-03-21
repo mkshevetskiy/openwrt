d3a9a4fad8e4e4b506574feda6999fa4dde17681
Christian Marangi <ansuelsmth@gmail.com>
2025-03-21 02:26:54 +0100

    airoha: add support for U-Boot
    Add support for U-Boot package. Currently the ATF is shipped precompiled
    but it's planned to compile it from source.
    
    The final bootloader image needs to be offset to 0x800 as the BOOTROM
    assume bl2 is placed at that fixed location.
    
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

--- target/linux/airoha/image/an7581.mk
+++ target/linux/airoha/image/an7581.mk
@@ -1,3 +1,8 @@
+define Build/an7581-bl2-bl31-uboot
+  head -c $$((0x800)) /dev/zero > $@
+  cat $(STAGING_DIR_IMAGE)/an7581_$1-u-boot.fip >> $@
+endef
+
 define Device/FitImageLzma
 	KERNEL_SUFFIX := -uImage.itb
 	KERNEL = kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(DEVICE_DTS).dtb
@@ -15,6 +20,8 @@ define Device/airoha_an7581-evb
   DEVICE_DTS_CONFIG := config@1
   KERNEL_LOADADDR := 0x80088000
   IMAGE/sysupgrade.bin := append-kernel | pad-to 128k | append-rootfs | pad-rootfs | append-metadata
+  ARTIFACT/bl2-bl31-uboot.bin := an7581-bl2-bl31-uboot rfb
+  ARTIFACTS := bl2-bl31-uboot.bin
 endef
 TARGET_DEVICES += airoha_an7581-evb
 
@@ -38,6 +45,8 @@ define Device/airoha_an7581-evb-emmc
   DEVICE_DTS := an7581-evb-emmc
   DEVICE_DTS_DIR := ../dts
   DEVICE_PACKAGES := kmod-i2c-an7581
+  ARTIFACT/bl2-bl31-uboot.bin := an7581-bl2-bl31-uboot rfb
+  ARTIFACTS := bl2-bl31-uboot.bin
 endef
 TARGET_DEVICES += airoha_an7581-evb-emmc
 
