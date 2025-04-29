1dea5955fb81c07623ee8e0433d95bde3aff2299
Christian Marangi <ansuelsmth@gmail.com>
2025-04-29 13:54:05 +0200

    airoha: add artifacts entry to generate final bootloader image
    Add artifacts entry to generate final bootloader image similar to how
    it's done for AN7581.
    
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

--- target/linux/airoha/image/an7583.mk
+++ target/linux/airoha/image/an7583.mk
@@ -1,3 +1,8 @@
+define Build/an7583-bl2-bl31-uboot
+  head -c $$((0x800)) /dev/zero > $@
+  cat $(STAGING_DIR_IMAGE)/an7583_$1-u-boot.fip >> $@
+endef
+
 define Device/FitImageLzma
 	KERNEL_SUFFIX := -uImage.itb
 	KERNEL = kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(DEVICE_DTS).dtb
@@ -14,5 +19,7 @@ define Device/airoha_an7583-evb
   DEVICE_DTS_CONFIG := config@1
   KERNEL_LOADADDR := 0x80088000
   IMAGE/sysupgrade.bin := append-kernel | pad-to 128k | append-rootfs | pad-rootfs | append-metadata
+  ARTIFACT/bl2-bl31-uboot.bin := an7583-bl2-bl31-uboot rfb
+  ARTIFACTS := bl2-bl31-uboot.bin
 endef
 TARGET_DEVICES += airoha_an7583-evb
